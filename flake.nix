6776f2f3b50603b79b342ed7e1f1a62875cf8832
ckie <git-525ff67@ckie.dev>
2022-05-02 18:49:04 +0300

    attempt

--- flake.nix
+++ flake.nix
@@ -2,53 +2,20 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url =
-      "github:nixos/nixpkgs/nixos-unstable"; # We want to use packages from the binary cache
-    flake-utils.url = "github:numtide/flake-utils";
+    # nixpkgs.url =
+    #   "github:nixos/nixpkgs/nixos-unstable"; # We want to use packages from the binary cache
+    inputs.dream2nix.url = "github:nix-community/dream2nix";
+    # flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in rec {
-        packages = rec {
-          mastobooper = with pkgs;
-            rustPlatform.buildRustPackage rec {
-              pname = "mastobooper";
-              version = "0.1.0";
-
-              src = ./.;
-
-              buildInputs = [ alsaLib ];
-              nativeBuildInputs = [ pkg-config ];
-              cargoSha256 =
-                "sha256-sG1TfIt2cC1tHTU7aMzRSk0z7dOVLQUZ7nozQVtKfdA=";
-
-              meta = with lib; {
-                description = "mastobooper";
-                license = licenses.unlicense;
-              };
-            };
-          default = mastobooper;
-        };
-
-        defaultPackage = packages.default;
-
-        apps = rec {
-          mastobooper = flake-utils.lib.mkApp {
-            name = "mastobooper";
-            drv = packages.mastobooper;
-          };
-          default = mastobooper;
-        };
-
-        defaultApp = apps.default;
-
-        devShell = pkgs.mkShell {
-          CARGO_INSTALL_ROOT = "${toString ./.}/.cargo";
-
-          nativeBuildInputs = with pkgs; [ pkg-config ];
-          buildInputs = with pkgs; [ cargo rustc git alsaLib ];
-        };
-      });
+  outputs = inputs@{ self, dream2nix, ... }:
+    let
+      project = inputs.dream2nix.lib.init {
+        # modify according to your supported systems
+        systems = [ "x86_64-linux" ];
+        config.projectRoot = ./.;
+      };
+    in project.makeFlakeOutputs {
+      source = ./.;
+    };
 }
