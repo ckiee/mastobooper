00f66775e12934774992c13c9d6c8e3fb5f05cc2
ckie <git-525ff67@ckie.dev>
2022-05-02 19:37:05 +0300

    okay i kinda fixed that but idrc not worth it still no defaultApp

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     # nixpkgs.url =
     #   "github:nixos/nixpkgs/nixos-unstable"; # We want to use packages from the binary cache
-    inputs.dream2nix.url = "github:nix-community/dream2nix";
+    dream2nix.url = "github:nix-community/dream2nix";
     # flake-utils.url = "github:numtide/flake-utils";
   };
 
